2fecb0394528a1ea7dbde44985d6de34b9ce27a7
alshehri12 <xmx1000@gmail.com>
2025-04-08 22:53:03 +0300

    Initial Commit

---
+++ SaudiRadio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaudiRadio
+//
+//  Created by Abdulrahman Alshehri on 10/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaudiRadio/SaudiRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaudiRadioApp.swift
+//  SaudiRadio
+//
+//  Created by Abdulrahman Alshehri on 10/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaudiRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
